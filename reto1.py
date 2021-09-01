98cc0c3488d242c3bae2414ed572b4eaa94dd807
Jorge Franco Dizu <86854606+JorgeFrancodizu@users.noreply.github.com>
2021-08-31 20:25:47 -0500

    Create reto1.py

---
+++ reto1.py
@@ -0,0 +1,20 @@
+def caracteristicas_musicales(p, t, bmp):
+    return resultado
+
+p = int(input())
+
+
+t = p*2+4
+bmp = (p+t)//5
+int = bmp
+
+print (p,t,bmp)
+
+if bmp <= 20:
+    print("Uno ")
+elif bmp  <= 30:
+    print ("dos")
+elif bmp <= 50:
+    print ("tres")
+elif bmp >= 51:
+    print ("cuatro")
